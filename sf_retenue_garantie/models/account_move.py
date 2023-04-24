d7be6200f3edab683d4fa9b920a1b6fc666f105a
salwa-ksila <ksila.salwa@gmail.com>
2023-04-24 16:50:50 +0100

    add changes in module

--- sf_retenue_garantie/models/account_move.py
+++ sf_retenue_garantie/models/account_move.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class AccountMove(models.Model):
@@ -11,13 +11,14 @@ class AccountMove(models.Model):
         self.guarantee_percentage = self.amount_total * 0.05
 
     def action_post(self):
+        due_date = fields.Date.context_today(self).replace(fields.Date.context_today(self).year + 1)
         res = super(AccountMove, self).action_post()
         vals = {
             'name': 'Draft',
             'invoice_number': self.name,
             'customer_id': self.partner_id.id,
             'amount': self.amount_total,
-            'due_date': fields.Date.context_today(self)
+            'due_date': due_date
         }
         self.env['sf.retenue.guarantee'].create(vals)
         return res
