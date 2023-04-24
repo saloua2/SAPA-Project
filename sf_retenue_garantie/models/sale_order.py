feeb14a6b21d6738895c31b7eeeeb0cd85c2bf87
Jawaher28 <Jawaher.amara@softina.tn>
2023-04-24 09:27:39 +0100

    jawaher modif

--- sf_retenue_garantie/models/sale_order.py
+++ sf_retenue_garantie/models/sale_order.py
@@ -5,6 +5,8 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     guarantee_percentage = fields.Float(compute='compute_guarantee_percentage')
+    guarantee_return = fields.Boolean(string="Retenue de Garantie")
+    prime = fields.Boolean(string="Prime CEE")
 
     @api.depends('amount_total')
     def compute_guarantee_percentage(self):
