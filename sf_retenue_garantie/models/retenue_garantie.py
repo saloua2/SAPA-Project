48b8a6abdc07295f0ca62ff8559bd5de262e1cbe
salwa-ksila <ksila.salwa@gmail.com>
2023-04-24 21:31:36 +0100

    changes in retenue

--- sf_retenue_garantie/models/retenue_garantie.py
+++ sf_retenue_garantie/models/retenue_garantie.py
@@ -4,6 +4,7 @@ from odoo import api, fields, models
 class RetenueGarantie(models.Model):
     _name = 'sf.retenue.guarantee'
     _description = 'Retenue de garantie'
+    _order = 'id desc'
 
     name = fields.Char('Retenue de ganatie')
     invoice_number = fields.Char('Numéro de la facture')
@@ -21,3 +22,13 @@ class RetenueGarantie(models.Model):
         tracking=True,
         default='draft',
     )
+    active = fields.Boolean(string="Active", default=True)
+
+    def action_confirm(self):
+        self.write({'state': 'confirmed'})
+
+    def reset_draft(self):
+        self.write({'state': 'draft'})
+
+    def action_cancel(self):
+        self.write({'state': 'cancel'})
